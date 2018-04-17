766d7aa1ea3f009bb0be7a14bed5c7092996b8c7
Mischii <38343505+Mischii@users.noreply.github.com>
2018-04-17 08:16:43 +0200

    color change

--- src/schiffeversenken/Feld.java
+++ src/schiffeversenken/Feld.java
@@ -20,7 +20,7 @@ public class Feld {
 		break;
 		case(3): parent.fill(112,112,112);  //Ein Teil eines Schiffes
 		break;
-		case(4): parent.fill(255,255,255);  //Wasser das  besetzt ist, damit der Gegner die SChiffe nicht sieht
+		case(4): parent.fill(30,125,230);  //Wasser das  besetzt ist, damit der Gegner die SChiffe nicht sieht
 		break;
 		case(5): parent.fill(243, 187, 42); //Getroffen,nicht versenkt
 		break;
